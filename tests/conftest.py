6c7db6ac2b2bf49490948b6ee6573bf161a9b14c
Sean Quinlan <1011062+sbquinlan@users.noreply.github.com>
2023-04-11 10:12:42 -0700

    format

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,41 @@
 from pytest import fixture
 
-@fixture(scope='module')
+
+@fixture(scope="module")
 def example():
-  return [
-    {
-      'a': 1,
-      'b': 2,
-      'c': {
-        'd': 3,
-        'e': {
-          'f': 4
+    return [
+        {
+            "a": 1,
+            "b": 2,
+            "c": {"d": 3, "e": {"f": 4}, "g": 5},
         },
-        'g': 5
-      },
-    },
-    {
-      'a': 1, 
-      'b': 2, 
-      'c.d': 3, 
-      'c.e.f': 4, 
-      'c.g': 5
-    }
-  ]
+        {"a": 1, "b": 2, "c.d": 3, "c.e.f": 4, "c.g": 5},
+    ]
 
-@fixture(scope='module')
+
+@fixture(scope="module")
 def empty():
-  return [{}, {}]
+    return [{}, {}]
 
-@fixture(scope='module')
+
+@fixture(scope="module")
 def empty_key():
-  return [{'': { '': 'a' }}, {'.': 'a'}]
+    return [{"": {"": "a"}}, {".": "a"}]
 
-@fixture(scope='module')
+
+@fixture(scope="module")
 def escaped_key():
-  return [{'.': { '|' : 'a' }}, {'|..||': 'a'}]
+    return [{".": {"|": "a"}}, {"|..||": "a"}]
 
-@fixture(scope='module')
+
+@fixture(scope="module")
 def complex_keys():
-  return [{'.||.|': { '.||' : 'a', '|.|' : '||||' }}, {'|.|||||.||.|.||||': 'a', '|.|||||.||.|||.||': '||||'}]
+    return [
+        {".||.|": {".||": "a", "|.|": "||||"}},
+        {"|.|||||.||.|.||||": "a", "|.|||||.||.|||.||": "||||"},
+    ]
 
-@fixture(scope='module')
+
+@fixture(scope="module")
 def defaults(example, empty, empty_key, escaped_key, complex_keys):
-  return [example, empty, empty_key, escaped_key, complex_keys]
+    return [example, empty, empty_key, escaped_key, complex_keys]
--- tests/flatten_test.py
+++ tests/flatten_test.py
@@ -1,18 +1,12 @@
 from flatten import flatten_packet
 
+
 def test_defaults(defaults):
-  assert all(flatten_packet(original) == flattened for original, flattened in defaults)
+    assert all(
+        flatten_packet(original) == flattened for original, flattened in defaults
+    )
+
 
 def test_numbers():
-  packet = {
-    1: 1,
-    2: 2,
-    3: {
-      3: 3,
-      4: {
-        4: 4
-      },
-      5: 5
-    }
-  }
-  assert flatten_packet(packet) == {'1': 1, '2': 2, '3.3': 3, '3.4.4': 4, '3.5': 5}
+    packet = {1: 1, 2: 2, 3: {3: 3, 4: {4: 4}, 5: 5}}
+    assert flatten_packet(packet) == {"1": 1, "2": 2, "3.3": 3, "3.4.4": 4, "3.5": 5}
